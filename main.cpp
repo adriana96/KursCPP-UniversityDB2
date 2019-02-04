f94362388270a8b7abf3085c29102e6c91b1ec73
Jakub Zych <jakub.zych@tieto.com>
2019-02-04 14:11:00 +0100

    Main: Remove menu from project

--- main.cpp
+++ main.cpp
@@ -4,38 +4,9 @@
 
 using namespace std;
 
-#define MENU_SHOW               1
-#define MENU_NEW_STUDENT        2
-#define MENU_NEW_EMPLOYEE       3
-#define MENU_SEARCH_NAME        4
-#define MENU_SEARCH_PESEL       5
-#define MENU_REMOVE_PESEL       6
-#define MENU_MODIFY_PESEL       7
-#define MENU_SORT_SALARY        8
-#define MENU_SORT_PESEL         9
-#define MENU_SORT_NAME          10
-#define MENU_EXIT               11
-
 #define DATABASE_FILENAME "db.dat"
 
-void menuShow() {
-    cout << "Program option:"   << endl;
-    cout << MENU_SHOW           << ". Show database content" << endl;
-    cout << MENU_NEW_STUDENT    << ". Add new student" << endl;
-    cout << MENU_NEW_EMPLOYEE   << ". Add new employee" << endl;
-    cout << MENU_SEARCH_NAME    << ". Search by last name" << endl;
-    cout << MENU_SEARCH_PESEL   << ". Search by PESEL" << endl;
-    cout << MENU_REMOVE_PESEL   << ". Remove by PESEL" << endl ;
-    cout << MENU_MODIFY_PESEL   << ". Modify emplyee by PESEL" << endl;
-    cout << MENU_SORT_SALARY    << ". Sort by salary" << endl;
-    cout << MENU_SORT_PESEL     << ". Sort by PESEL" << endl;
-    cout << MENU_SORT_NAME      << ". Sort by last name" << endl;
-    cout << MENU_EXIT           << ". Exit" << endl;
-}
-
 int main() {
-    int option;
-
     // Clear terminal
     cout << "\033[2J\033[1;1H";
 
@@ -50,54 +21,7 @@ int main() {
 
     Database* db = new Database();
     db->loadFromFile(DATABASE_FILENAME);
-
-    do {
-        menuShow();
-        cin >> option;
-
-        switch (option) {
-            case MENU_SHOW :
-                cout << "Show database content" << endl;
-                db->showAll();
-                break;
-            case MENU_NEW_STUDENT :
-                cout << "Add new student" << endl;
-                break;
-            case MENU_NEW_EMPLOYEE :
-                cout << "Add new employee" << endl;
-                break;
-            case MENU_SEARCH_NAME :
-                cout << "Search by last name" << endl;
-                break;
-            case MENU_SEARCH_PESEL :
-                cout << "Search by PESEL" << endl;
-                break;
-            case MENU_REMOVE_PESEL :
-                cout << "Remove by PESEL" << endl ;
-                break;
-            case MENU_MODIFY_PESEL :
-                cout << "Modify emplyee by PESEL" << endl;
-                break;
-            case MENU_SORT_SALARY :
-                cout << "Sort by salary" << endl;
-                break;
-            case MENU_SORT_PESEL :
-                cout << "Sort by PESEL" << endl;
-                break;
-            case MENU_SORT_NAME :
-                cout << "Sort by last name" << endl;
-                break;
-            case MENU_EXIT :
-                cout << "Exit (save to file)" << endl;
-                db->saveToFile(DATABASE_FILENAME);
-                break;
-            default :
-                cout << "Unknown option " << option << "!" << endl;
-                break;
-        }
-
-        cout << endl;
-    } while (option != MENU_EXIT);
+    db->saveToFile(DATABASE_FILENAME);
 
     return 0;
 }
